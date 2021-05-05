a68d74063b1705fbd7614ea68670c9827c5283b8
alexcientista <alexandrecientista@gmail.com>
2021-05-05 12:12:52 -0300

    criação de classe e métodos

--- básico de Python/pythonProject1_comDjango/pythonProject1_comDjango/pessoa.py
+++ básico de Python/pythonProject1_comDjango/pythonProject1_comDjango/pessoa.py
@@ -1,4 +1,8 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=None):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self, num):
         return f"Olá {id(self)} , {num}"
 
@@ -6,8 +10,15 @@ class Pessoa:
         print(f'Seja bem-vindo {contratado}, ctps: {ctps}')
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p, 83))
-    print(p.contratar('Alex', 123456798))
+    p = Pessoa('Alex')
+    print(p.nome)
+
+    #print(Pessoa.cumprimentar(p, 83))
+    #print(p.contratar('Alex', 123456798))
+    print(p.nome)
+    p.nome = "Renzo"
+    p.idade = 35
+    print(f'{p.nome}, {p.idade}')
+
 
 
